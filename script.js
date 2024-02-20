b37310e86c5cb4f9f4e7956c59cd9e00a67b7375
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2024-02-20 19:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,125 +1,21 @@
 function calculateMinCost() {
-    let inputElement = document.querySelector('#rope-lengths');
-    let inputValue = inputElement.value;
-    let ropeLengths = inputValue.split(',').map(Number);
+  //your code here
+  let string = document.getElementById("rope-lengths").value
 
-    class MinHeap {
-        constructor() {
-            this.heap = [];
-        }
+	let arr = string.split(",");
+	let finalValue =0;
+	while(arr.length > 1){
+		arr.sort((a,b) =>{return(a-b)});
+		let value = parseInt (arr.shift());
+		let value1 = parseInt(arr.shift());
+		let mainValue = value+value1;
+		finalValue = finalValue + mainValue;
+		arr.push(mainValue);
 
-        getParentIndex(i) {
-            return Math.floor((i - 1) / 2);
-        }
+	}
+  let result = document.getElementById("result");
+	result.innerText = finalValue;
+	return finalValue;
 
-        getLeftChildIndex(i) {
-            return 2 * i + 1;
-        }
-
-        getRightChildIndex(i) {
-            return 2 * i + 2;
-        }
-
-        hasParent(i) {
-            return this.getParentIndex(i) >= 0;
-        }
-
-        hasLeftChild(i) {
-            return this.getLeftChildIndex(i) < this.heap.length;
-        }
-
-        hasRightChild(i) {
-            return this.getRightChildIndex(i) < this.heap.length;
-        }
-
-        parent(i) {
-            return this.heap[this.getParentIndex(i)];
-        }
-
-        leftChild(i) {
-            return this.heap[this.getLeftChildIndex(i)];
-        }
-
-        rightChild(i) {
-            return this.heap[this.getRightChildIndex(i)];
-        }
-
-        swap(i, j) {
-            let temp = this.heap[i];
-            this.heap[i] = this.heap[j];
-            this.heap[j] = temp;
-        }
-
-        peek() {
-            if (this.heap.length == 0) {
-                throw "Heap is empty";
-            } else {
-                return this.heap[0];
-            }
-        }
-
-        poll() {
-            if (this.heap.length == 0) {
-                throw "Heap is empty";
-            } else {
-                let item = this.heap[0];
-                this.heap[0] = this.heap.pop();
-                this.heapifyDown();
-                return item;
-            }
-        }
-
-        add(item) {
-            this.heap.push(item);
-            this.heapifyUp();
-        }
-
-        heapifyUp() {
-            let index = this.heap.length - 1;
-            while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
-                this.swap(this.getParentIndex(index), index);
-                index = this.getParentIndex(index);
-            }
-        }
-
-        heapifyDown() {
-            let index = 0;
-            while (this.hasLeftChild(index)) {
-                let smallerChildIndex = this.getLeftChildIndex(index);
-                if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
-                    smallerChildIndex = this.getRightChildIndex(index);
-                }
-
-                if (this.heap[index] < this.heap[smallerChildIndex]) {
-                    break;
-                } else {
-                    this.swap(index, smallerChildIndex);
-                }
-                index = smallerChildIndex;
-            }
-        }
-    }
-
-    // Calculate minimum cost of ropes using heap
-
-    let minHeap = new MinHeap();
-
-    // Add all rope lengths to the heap
-    for (let length of ropeLengths) {
-        minHeap.add(length);
-    }
-
-    let totalCost = 0;
-
-    // Keep connecting ropes until there is only one rope left
-    while (minHeap.heap.length > 1) {
-        let firstRope = minHeap.poll();
-        let secondRope = minHeap.poll();
-        let combinedCost = firstRope + secondRope;
-        totalCost += combinedCost;
-        minHeap.add(combinedCost);
-    }
-
-    document.getElementById('result').innerText = totalCost;
 }
 
